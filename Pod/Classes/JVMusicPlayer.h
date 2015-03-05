febaf6c3c2633926f7eb3036a7c42280d199d837
Jorge Valbuena <jorgevalbuena2@gmail.com>
2015-03-04 19:49:30 -0500

    added class to the project

---
+++ Pod/Classes/JVMusicPlayer.h
@@ -0,0 +1,13 @@
+//
+//  JVMusicPlayer.h
+//  JVMusicPlayer
+//
+//  Created by Jorge Valbuena on 2015-03-04.
+//  Copyright (c) 2015 Jorge Valbuena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JVMusicPlayer : UIView
+
+@end
---
+++ Pod/Classes/JVMusicPlayer.m
@@ -0,0 +1,15 @@
+//
+//  JVMusicPlayer.m
+//  JVMusicPlayer
+//
+//  Created by Jorge Valbuena on 2015-03-04.
+//  Copyright (c) 2015 Jorge Valbuena. All rights reserved.
+//
+
+#import "JVMusicPlayer.h"
+
+@implementation JVMusicPlayer
+
+
+
+@end
